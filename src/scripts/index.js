d997ced67a237e4e57bfb9d8c85275e25b05e400
Bulat Sitdikov <rdvad2@gmail.com>
2023-06-23 13:19:04 +0500

    add plantattion

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,22 @@
 import takeControlSelect from "./ElementsControllers/SelectController";
 
 takeControlSelect();
+
+//swipers init
+const firstScreenSwiper = new Swiper ('.first-screen', {
+	pagination: {
+		el: '.first-screen .swiper-pagination',
+		type: 'bullets',
+		clickable: true
+	},
+	loop: true,
+	slidesPerView: 1,
+	watchOverflow: true,
+	slidesPerGroup: 1,
+	autoplay: {
+		delay: 5000,
+		stopOnLastSlide: false,
+		disableOnInteraction: false
+	},
+	grabCursor: true,
+})
