cef5cbc1748523a304137b114705c5c67533a738
v.grigoriev1 <v.grigoriev@wptt.ru>
2023-07-18 17:59:03 +0500

    feat: timer

--- src/scripts/ElementsControllers/TimerController.js
+++ src/scripts/ElementsControllers/TimerController.js
@@ -1,19 +1,38 @@
 const takeControlTimer = () => {
 
-	const timerElement = document.querySelector('.timer_link')
+	const timerLink = document.querySelector('.timer_link')
+	const timer = document.querySelector('#timer')
+	let time
 
-	if (timerElement !== undefined) {
-		timerElement.classList.remove('_active')
+	if (timer !== undefined) {
+		const outTimer = () => {
+			timerLink.classList.add('_disabled')
+			timerLink.classList.remove('_active')
 
-		setTimeout(function () {
-			timerElement.classList.add('_active')
-		}, 5000)
-	}
+			time = 11
 
-	
+			timer.textContent = " 0:" + time
 
+			setInterval(() => {
+				if (time <= 0) {
+					timerLink.classList.remove('_disabled')
+					timerLink.classList.add('_active')
+				} else if (time <= 10) {
+					timer.textContent = " 0:0" + --time
+				} else {
+					timer.textContent = " 0:" + --time
+				}			
+			}, 1000)
+		}	
+		
 
-	
+		timerLink.addEventListener("click", (e) => {
+			e.preventDefault()
+			outTimer()
+			console.log(time)
+		})
+		
+	}	
 }
 
 export default takeControlTimer
